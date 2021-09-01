6c6570d6d28b3b38d81f143592d8358f7e1b808c
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2021-09-01 22:49:41 +0900

    Updating

--- Engine/Camera.h
+++ Engine/Camera.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "framework.h"
+
 class Camera
 {
+	XMMATRIX View;
 };
 
--- Engine/Texture.h
+++ Engine/Texture.h
@@ -1,5 +1,9 @@
 #pragma once
+#include "framework.h"
+
 class Texture
 {
+	ID3D11ShaderResourceView* textureRV;
+	ID3D11SamplerState* samplerState;
 };
 
