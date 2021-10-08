10d5ee79d346d1b1f312d71cc2b3c09b07e5e007
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2021-10-08 22:55:24 +0900

    Updating Buffer

--- Engine/Buffer.h
+++ Engine/Buffer.h
@@ -7,5 +7,10 @@ public:
 	ID3D11InputLayout* vertexLayout;
 	ID3D11Buffer* vertexBuffer;
 	ID3D11Buffer* indexBuffer;
+
+	~Buffer();
+
+	void Update();
+	void Render(ID3D11DeviceContext* context);
 };
 
